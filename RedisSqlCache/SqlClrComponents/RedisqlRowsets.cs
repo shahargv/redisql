be65b2c79f2cf1639a2ee0c764f8aaa141d5845e
Shahar Gvirtz <shahar@ishahar.net>
2015-10-29 12:35:10 +0200

    fix: dealing with queries that has 0 rows returned - metadata still stored

--- RedisSqlCache/SqlClrComponents/RedisqlRowsets.cs
+++ RedisSqlCache/SqlClrComponents/RedisqlRowsets.cs
@@ -18,29 +18,7 @@ namespace RediSql.SqlClrComponents
         public static void StoreQueryResultsData(string host, int port, string password, int? dbId, string key,
             string query, TimeSpan? expiration, bool replaceExisting)
         {
-            List<string> valuesToAdd = new List<string>();
-            using (SqlConnection connection = new SqlConnection("context connection=true"))
-            {
-                connection.Open();
-                using (SqlCommand queryCommand = new SqlCommand(query, connection))
-                using (SqlDataReader rowsetReader = queryCommand.ExecuteReader())
-                {
-                    while (rowsetReader.Read())
-                    {
-                        if (valuesToAdd.Count == 0)
-                        {
-                            valuesToAdd.Add(GetColumnMetadataXml(rowsetReader));
-                        }
-                        XElement el = new XElement("item");
-                        for (int index = 0; index < rowsetReader.FieldCount; index++)
-                        {
-                            if (!rowsetReader.IsDBNull(index))
-                                el.Add(new XElement(rowsetReader.GetName(index), rowsetReader.GetValue(index)));
-                        }
-                        valuesToAdd.Add(el.ToString());
-                    }
-                }
-            }
+            var valuesToAdd = ExecuteQueryAndGetResultList(query);
             using (var redis = RedisConnection.GetConnection(host, port, password, dbId))
             {
                 if (redis.ContainsKey(key))
@@ -62,22 +40,50 @@ namespace RediSql.SqlClrComponents
             }
         }
 
-        private static string GetColumnMetadataXml(SqlDataReader rowsetReader)
+        private static List<string> ExecuteQueryAndGetResultList(string query)
         {
-
-            var schemaTable = rowsetReader.GetSchemaTable();
-
-
-            XElement el = new XElement("ColumnsMetadata");
-            foreach (DataRow row in schemaTable.Rows)
+            List<string> valuesToAdd = new List<string>();
+            using (SqlConnection connection = new SqlConnection("context connection=true"))
             {
-                el.Add(new XElement("Column",
-                                            new XAttribute("order", row["ColumnOrdinal"]),
-                                            new XAttribute("name", row["ColumnName"]),
-                                            new XAttribute("sqlType", row["DataTypeName"]),
-                                            new XAttribute("size", row["ColumnSize"])));
+                connection.Open();
+                valuesToAdd.Add(GetColumnMetadataXml(connection, query));
+                using (SqlCommand queryCommand = new SqlCommand(query, connection))
+                using (SqlDataReader rowsetReader = queryCommand.ExecuteReader())
+                {
+                    while (rowsetReader.Read())
+                    {
+                        XElement el = new XElement("item");
+                        for (int index = 0; index < rowsetReader.FieldCount; index++)
+                        {
+                            if (!rowsetReader.IsDBNull(index))
+                                el.Add(new XElement(rowsetReader.GetName(index), rowsetReader.GetValue(index)));
+                        }
+                        valuesToAdd.Add(el.ToString());
+                    }
+                }
             }
-            return el.ToString();
+            return valuesToAdd;
+        }
+
+        private static string GetColumnMetadataXml(SqlConnection connection, string query)
+        {
+            using (var cmd = new SqlCommand("sp_describe_first_result_set", connection))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("tsql", query);
+                XElement el = new XElement("ColumnsMetadata");
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        el.Add(new XElement("Column",
+                            new XAttribute("order", reader["column_ordinal"]),
+                            new XAttribute("name", reader["name"]),
+                            new XAttribute("sqlType", reader["system_type_name"])));
+                    }
+                }
+                return el.ToString();
+            }
         }
     }
 }
